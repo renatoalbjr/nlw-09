2253226c85b8f6130d5afb65d8ed5800d91d41f9
Renato <rejunior147@gmail.com>
2022-09-17 14:21:03 -0300

    Finished 5th lesson

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -6,9 +6,15 @@ import {
   Inter_900Black,
   useFonts,
 } from "@expo-google-fonts/inter";
+import { Subscription } from "expo-modules-core";
+import { useEffect, useRef } from "react";
+import * as Notifications from "expo-notifications";
 
 import { Loading } from "./src/components/Loading";
 import { Routes } from "./src/routes";
+import { getPushNotificationToken } from "./src/services/getPushNotificationToken";
+
+import "./src/services/notificationConfigs";
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -18,5 +24,36 @@ export default function App() {
     Inter_700Bold,
     Inter_900Black,
   });
+
+  const getNotificationListener = useRef<Subscription>();
+  const responseNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+    getPushNotificationToken();
+  }, []);
+
+  useEffect(() => {
+    getNotificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        console.log(notification);
+      });
+
+    responseNotificationListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      if (getNotificationListener.current)
+        Notifications.removeNotificationSubscription(
+          getNotificationListener.current
+        );
+      if (responseNotificationListener.current)
+        Notifications.removeNotificationSubscription(
+          responseNotificationListener.current
+        );
+    };
+  });
+
   return fontsLoaded ? <Routes /> : <Loading />;
 }
